263354170c5d7474ba21f6bff0816c6fdc722f5a
ant <theant@mail.ru>
2022-08-12 13:24:14 +0400

    Initial commit

---
+++ app/src/main/java/com/camcorderemulator/data/Camcorder.kt
@@ -0,0 +1,10 @@
+package com.camcorderemulator.data
+
+data class Camcorder(
+    val id: Int,
+    val name: String,
+    val image: String,
+    val vendorCode: String,
+    val cost: Float
+) {
+}
