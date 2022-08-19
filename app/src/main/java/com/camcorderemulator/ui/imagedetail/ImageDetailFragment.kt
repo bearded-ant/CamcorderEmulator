83bbbf702d4fc774297500cf886fe1afdff47c9b
ant <theant@mail.ru>
2022-08-19 23:16:01 +0400

    add navigation from gallery to image detail fragment add zoom image in detail fragment

--- app/src/main/java/com/camcorderemulator/ui/imagedetail/ImageDetailFragment.kt
+++ app/src/main/java/com/camcorderemulator/ui/imagedetail/ImageDetailFragment.kt
@@ -7,17 +7,18 @@ import android.view.ScaleGestureDetector
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.gbhomework.camcorderemulator.R
 import com.gbhomework.camcorderemulator.databinding.FragmentImageDetailBinding
 import com.squareup.picasso.Picasso
+import java.io.File
 
-class ImageDetailFragment : Fragment(){
+class ImageDetailFragment : Fragment() {
 
     private var _binding: FragmentImageDetailBinding? = null
     private val binding
         get() = _binding!!
 
     private var scalingDetector: ScaleGestureDetector? = null
+    private var pathArgument: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,6 +26,7 @@ class ImageDetailFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentImageDetailBinding.inflate(layoutInflater)
+        pathArgument = ImageDetailFragmentArgs.fromBundle(requireArguments()).imgPath
         return binding.root
     }
 
@@ -32,14 +34,17 @@ class ImageDetailFragment : Fragment(){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        scalingDetector = ScaleGestureDetector(requireContext(), ScalingListener(1f, binding.frImageDetailPicture))
+        scalingDetector = ScaleGestureDetector(
+            requireContext(),
+            ScalingListener(1f, binding.frImageDetailPicture)
+        )
         binding.frImageDetailPicture.setOnTouchListener { _, motionEvent ->
             scalingDetector!!.onTouchEvent(motionEvent)
             true
         }
 
-//        val imageFile = File("string")
-//        if (imageFile.exists())
-        Picasso.get().load(R.drawable.svis123asdslv2).into(binding.frImageDetailPicture)
+        val imageFile = File(pathArgument)
+        if (imageFile.exists())
+            Picasso.get().load(imageFile).into(binding.frImageDetailPicture)
     }
 }
