4be81087c292d592706ba430e19bafc1487f9f1a
ant <theant@mail.ru>
2022-08-20 01:58:12 +0400

    add metadata dependencies and implementation

--- app/src/main/java/com/camcorderemulator/ui/imagedetail/ImageDetailFragment.kt
+++ app/src/main/java/com/camcorderemulator/ui/imagedetail/ImageDetailFragment.kt
@@ -1,11 +1,13 @@
 package com.camcorderemulator.ui.imagedetail
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ScaleGestureDetector
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.drew.imaging.ImageMetadataReader
 import com.gbhomework.camcorderemulator.databinding.FragmentImageDetailBinding
 import com.squareup.picasso.Picasso
 import java.io.File
@@ -33,7 +35,18 @@ class ImageDetailFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val imageFile = File(pathArgument)
-        if (imageFile.exists())
+        if (imageFile.exists()) {
             Picasso.get().load(imageFile).into(binding.frImageDetailPicture)
+            val metadata = ImageMetadataReader.readMetadata(imageFile)
+
+            for (meta in metadata.directories) {
+                for (tag in meta.tags) {
+                    Log.d("myLogs", "${tag.tagType} ${tag.tagName} ${tag.description}")
+                    binding.frImageDetailMetadata.text =
+                        "${tag.tagType} ${tag.tagName} ${tag.description}"
+                }
+            }
+
+        }
     }
 }
